2ffe4ad7cd32ff58f7c16dc10065d678539df131
Lorrany <lolo.souza80@yahoo.com>
2022-02-21 22:45:43 -0300

    Adicao de comentarios. Co-authored-by: daniso123<dannysoaresdeoliveira@gmail.com>

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ onload = function () {
     const solve = document.getElementById('solve');
     const temptext = document.getElementById('temptext');
     const temptext2 = document.getElementById('temptext2');
-    const cities = ['Brasília','São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba', 'Porto Alegre', 'Fortaleza', 'Goiânia', 'Santos', 'Recife'];
+    const cities = ['Brasília', 'São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba', 'Porto Alegre', 'Fortaleza', 'Goiânia', 'Santos', 'Recife'];
 
-    
+     // inicializa as opções do gráfico
     const options = {
         edges: {
             labelHighlightBold: true,
@@ -34,30 +34,33 @@ onload = function () {
         }
     };
 
-    
+    // Inicialize sua rede!
+    // Rede para o gráfico de perguntas
     const network = new vis.Network(container);
     network.setOptions(options);
-    // Network for result graph
+    // Rede para o gráfico de resultados
     const network2 = new vis.Network(container2);
     network2.setOptions(options);
-
+ 
     function createData() {
-        V = Math.floor(Math.random() * 8) + 3; 
+        V = Math.floor(Math.random() * 8) + 3; // Garante que V esteja entre 3 e 10
         let nodes = [];
         for (let i = 1; i <= V; i++) {
             nodes.push({ id: i, label: cities[i - 1] })
         }
-        
+        // Prepara nós de estilo vis.js para nossos dados
         nodes = new vis.DataSet(nodes);
 
-        
+         // Criando uma árvore como estrutura de gráfico subjacente
         let edges = [];
         for (let i = 2; i <= V; i++) {
             let neigh = i - Math.floor(Math.random() * Math.min(i - 1, 3) + 1); // Picks a neighbour from i-3 to i-1
             edges.push({ type: 0, from: i, to: neigh, color: 'orange', label: String(Math.floor(Math.random() * 70) + 31) });
         }
 
-        
+        // Adicionando aleatoriamente novas arestas ao gráfico
+        // O tipo de Ônibus é 0
+        // Tipo de Avião é 1
         for (let i = 1; i <= V / 2;) {
 
             let n1 = Math.floor(Math.random() * V) + 1;
@@ -68,7 +71,8 @@ onload = function () {
                     n1 = n2;
                     n2 = tmp;
                 }
-                
+                 //Ver se já existe uma aresta entre esses dois vértices
+                // E se isso acontecer, então de que tipo
                 let works = 0;
                 for (let j = 0; j < edges.length; j++) {
                     if (edges[j]['from'] === n1 && edges[j]['to'] === n2) {
@@ -78,10 +82,12 @@ onload = function () {
                             works = 2;
                     }
                 }
-
+// Adicionando arestas ao gráfico
+                // If works == 0, você pode adicionar Ônibus e Avião entre os vértices
+                // If works == 1, você só pode adicionar Avião entre eles
                 if (works <= 1) {
                     if (works === 0 && i < V / 4) {
-                       
+                       // Adicionando um Ônibus 
                         edges.push({
                             type: 0,
                             from: n1,
@@ -90,7 +96,7 @@ onload = function () {
                             label: String(Math.floor(Math.random() * 70) + 31)
                         });
                     } else {
-                       
+                       // Adicionando um Avião
                         edges.push({
                             type: 1,
                             from: n1,
@@ -104,7 +110,7 @@ onload = function () {
             }
         }
 
-        
+         // Configurando os novos valores das variáveis ​​globais
         src = 1;
         dst = V;
         curr_data = {
@@ -114,7 +120,7 @@ onload = function () {
     }
 
     genNew.onclick = function () {
-        
+        // Cria novos dados e exibe os dados
         createData();
         network.setData(curr_data);
         temptext2.innerText = 'Encontre o menor tempo de viagem de ' + cities[src - 1] + ' para ' + cities[dst - 1];
@@ -125,7 +131,7 @@ onload = function () {
     };
 
     solve.onclick = function () {
-       
+       // Cria gráfico a partir de dados e configura para exibição
         temptext.style.display = "none";
         temptext2.style.display = "none";
         container2.style.display = "inline";
@@ -179,17 +185,17 @@ onload = function () {
 
         const data = curr_data;
 
-        
+        // Criando um gráfico de matriz de lista de adjacências a partir de dados de perguntas
         const graph = createGraph(data);
 
-        
+        // Aplicando djikstra de src e dst
         let dist1 = djikstra(graph, V, src - 1);
         let dist2 = djikstra(graph, V, dst - 1);
 
-        
+        // Inicializa min_dist para min distance via Ônibus de src para dst
         let mn_dist = dist1[dst - 1][0];
 
-        
+        // Veja se o avião deve ser usado
         let { plane, p1, p2 } = shouldTakePlane(data['edges'], dist1, dist2, mn_dist);
 
         let new_edges = [];
